a690527391f8d1ba3b17b6fdfbfc6485c52da26b
officialODBIT <contact@odbit.org>
2025-03-23 18:44:55 -0400

    Create sentry_77839.js

---
+++ sentry_77839.js
@@ -0,0 +1,31 @@
+        console.log(`
+          ██████████    ██████████      ████████      ██████████     ██
+        ██                  ██       ██         ██    ██       ██    ██
+        ██                  ██       ██         ██    ██████████     ██
+          █████████         ██       ██         ██    ██             ██
+                   ██       ██       ██         ██    ██             ██
+                   ██       ██       ██         ██    ██ 
+          ████████          ██         █████████      ██             ██
+
+        ENTERING SENSITIVE SCRIPTS OR COPYING AND PASTING SENSITIVE INFO
+        TO OTHERS WILL CAUSE YOUR
+        ACCOUNT TO BE HACKED! PLEASE DO NOT SEND ANY SPECID's OR PINS.
+        `)
+        console.warn(`
+          ██████████    ██████████      ████████      ██████████     ██
+        ██                  ██       ██         ██    ██       ██    ██
+        ██                  ██       ██         ██    ██████████     ██
+          █████████         ██       ██         ██    ██             ██
+                   ██       ██       ██         ██    ██             ██
+                   ██       ██       ██         ██    ██ 
+          ████████          ██         █████████      ██             ██
+
+        ENTERING SENSITIVE SCRIPTS OR COPYING AND PASTING SENSITIVE INFO 
+        TO OTHERS WILL CAUSE YOUR
+        ACCOUNT TO BE HACKED! PLEASE DO NOT SEND ANY SPECID's OR PINS.
+        `);
+
+        console.log('Loaded on local port host; 3000');
+        console.warn('loaded v0 --still in beta, break it and i will ban you. yes im talking to you person who took the webhook url and started saying racist stuff');
+        console.log("%cODBIT", "font-size: 7em; color: #640670; font-weight: bold; text-shadow: -2px -2px 0 black, 2px -2px 0 black, -2px 2px 0 black, 2px 2px 0 black;");
+        console.log("%DO NOT PASTE ANYTHING IN THIS CONSOLE UNLESS YOU KNOW WHAT YOU ARE DOING!", "font-size: 3em; color: #640670; font-weight: bold; text-shadow: -2px -2px 0 black, 2px -2px 0 black, -2px 2px 0 black, 2px 2px 0 black;");
