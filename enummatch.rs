26e88c05047e338befd13c0b4ada644debec960b
officialODBIT <contact@odbit.org>
2025-04-18 10:53:09 -0400

    Update enummatch.rs

--- enummatch.rs
+++ enummatch.rs
@@ -1,1 +1,16 @@
+enum Status {
+    Success,
+    Error,
+}
 
+fn check(status: Status) {
+    match status {
+        Status::Success => println!("Operation succeeded"),
+        Status::Error => println!("Operation failed"),
+    }
+}
+
+fn main() {
+    let status = Status::Success;
+    check(status);
+}
